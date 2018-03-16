e6f102d637691839f389c769bbda18e824cddbe6
Dustin Fehlman <df1837@gmail.com>
2018-03-16 02:47:09 -0700

    Added Card class.

--- A3.java
+++ A3.java
@@ -14,6 +14,42 @@ class Card {
     private char value;
     private Suit suit;
     private boolean errorFlag;
+    
+    private enum Suit {clubs, diamonds, hearts, spades}
+    
+    Card(char value, Suit suit){
+        
+    }
+    
+    public String toString() {
+        return "";
+    }
+    
+    public boolean equals(Card card) {
+        return false;
+    }
+    
+    public boolean isValid(char value, Suit suit) {
+        return false;
+    }
+    
+    //Mutators
+    public boolean set(char value, Suit suit) {
+        return false;
+    }
+    
+    //Accessors
+    public Suit getSuit() {
+        return this.suit;
+    }
+    
+    public char getValue() {
+        return this.value;
+    }
+    
+    public boolean getErrorFlag() {
+        return this.errorFlag;
+    }
 }
 
 class Hand {
