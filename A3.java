c74ed8cbc03c5451cd54b7251af4320fc988c01b
Dustin Fehlman <df1837@gmail.com>
2018-03-16 02:14:25 -0700

    starting layout

--- A3.java
+++ A3.java
@@ -1,4 +1,25 @@
+/* Decks of Cards Program
+ * Description: TBD
+ * By: John Seals, Hanna Bonert, Dustin Fehlman, and Carlos Sanchez 
+ */
 
 public class A3 {
+    
+    public static void main(String[] args) {
+        
+    }
+}
 
+class Card {
+    private char value;
+    private Suit suit;
+    private boolean errorFlag;
+}
+
+class Hand {
+    
+}
+
+class Deck {
+    
 }
