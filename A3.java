6676cc358a8dfd882a49bbaaa75d5855acf2ca64
Dustin Fehlman <df1837@gmail.com>
2018-03-16 02:58:53 -0700

    Added hand class outline

--- A3.java
+++ A3.java
@@ -21,15 +21,15 @@ class Card {
         
     }
     
+    private boolean isValid(char value, Suit suit) {
+        return false;
+    }
+  
     public String toString() {
         return "";
     }
     
     public boolean equals(Card card) {
-        return false;
-    }
-    
-    public boolean isValid(char value, Suit suit) {
         return false;
     }
     
@@ -53,7 +53,37 @@ class Card {
 }
 
 class Hand {
+    public final int MAX_CARDS = 100;
+    private Card[] myCards;
+    private int numCards;
     
+    Hand() {
+        
+    }
+    
+    public void resetHand() {
+        
+    }
+    
+    public boolean takeCard(Card card) {
+        return false;
+    }
+    
+    public Card playCard() {
+        return //card Object;
+    }
+    
+    public String toString() {
+        return "";
+    }
+    
+    public int getNumCards() {
+        return this.numCards;
+    }
+    
+    public Card inspectCard(int k) {
+        return //Card object
+    }
 }
 
 class Deck {
